baa2e3393b3603faee765075499eac81397ae119
Robin <robin99re@gmail.com>
2019-09-11 15:51:29 +0200

    Add new UI elements for the navigation Tab with Empty Sides

--- Kairos/UI/ShellPage.xaml.cs
+++ Kairos/UI/ShellPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Kairos.Business;
+using Kairos.UI.Settings;
+using Kairos.UI.WorkOverview;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -22,14 +25,48 @@ namespace Kairos
     /// </summary>
     public sealed partial class ShellPage : Page
     {
+        #region Fields
+        private List<PageItem> PageItems { get; set; }
+        #endregion
+
         public ShellPage()
         {
             this.InitializeComponent();
+            PageItems = new List<PageItem>();
+            PageItems.Add(new PageItem() { Name = "WorkOverview"  } );
+            Windows.UI.Core.SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = Windows.UI.Core.AppViewBackButtonVisibility.Collapsed;
+            Windows.UI.Core.SystemNavigationManager.GetForCurrentView().BackRequested += ShellPage_BackRequested;
+        }
+
+        private void ShellPage_BackRequested(object sender, Windows.UI.Core.BackRequestedEventArgs e)
+        {
+           
         }
 
         private void RootFrame_Navigated(object sender, NavigationEventArgs e)
         {
 
+        }
+
+        private void NavigationViewControl_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked)
+            {
+                frameMain.Navigate(typeof(SettingsPage));
+            }
+            else
+            {
+                string name = args.InvokedItem.ToString();
+
+                switch (name)
+                {
+                    case "Home":
+                        frameMain.Navigate(typeof(WorkOverviewPage));
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
