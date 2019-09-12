a7ce9b0b4107b3a78230bb68987057b700a8be41
Robin <robin99re@gmail.com>
2019-09-12 15:51:52 +0200

    Positioning in Map is available + New Controlls for Zomming added

--- Kairos/UI/Map/MapPage.xaml.cs
+++ Kairos/UI/Map/MapPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Maps;
@@ -26,13 +27,15 @@ namespace Kairos.UI.Map
     {
         #region Fields
         private BasicGeoposition _basicGeoposition;
+        private Geoposition _lastGeoPosition;
+        private bool _firstMeasure;
         #endregion
 
         public MapPage()
         {
             this.InitializeComponent();
             _basicGeoposition = new BasicGeoposition { Latitude = 51.314110, Longitude = 9.485962 };
-            AddSpaceNeedleIcon();
+            //AddSpaceNeedleIcon();
         }
 
         public void AddSpaceNeedleIcon()
@@ -70,14 +73,20 @@ namespace Kairos.UI.Map
         private async void Page_Loaded(object sender, RoutedEventArgs e)
         {
             var accessStatus = await Geolocator.RequestAccessAsync();
+            _firstMeasure = true;
 
             switch (accessStatus)
             {
                 case GeolocationAccessStatus.Unspecified:
                     break;
                 case GeolocationAccessStatus.Allowed:
-                    Geolocator geolocator = new Geolocator();
-                    geolocator.DesiredAccuracyInMeters = 10;
+                    Geolocator geolocator = new Geolocator
+                    {
+                        DesiredAccuracyInMeters = 10,
+                        ReportInterval = 2000
+                        
+                    };
+                    geolocator.PositionChanged += Geolocator_PositionChanged;
                     geolocator.StatusChanged += Geolocator_StatusChanged;
                     Geoposition pos = await geolocator.GetGeopositionAsync();
                     break;
@@ -88,15 +97,73 @@ namespace Kairos.UI.Map
             }
         }
 
-        private void Geolocator_StatusChanged(Geolocator sender, StatusChangedEventArgs args)
+        private async void Geolocator_PositionChanged(Geolocator sender, PositionChangedEventArgs args)
         {
-            throw new NotImplementedException();
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                var geoPoint = new BasicGeoposition();
+                geoPoint.Latitude = args.Position.Coordinate.Latitude;
+                geoPoint.Longitude = args.Position.Coordinate.Longitude;
+                UpdateLocation(geoPoint, false);
+            });
+            _lastGeoPosition = args.Position;
+            _firstMeasure = false;
         }
 
-        private void UpdateLocation(BasicGeoposition newPoint) 
+        private void Geolocator_StatusChanged(Geolocator sender, StatusChangedEventArgs args)
         {
-            //(myMap.MapElements[0] as MapIcon).Location.Position.Latitude = newPoint.Latitude;
-            //myMap.loca
+            
+        }
+
+        private void UpdateLocation(BasicGeoposition newPoint, bool center) 
+        {
+            if (_firstMeasure || _lastGeoPosition == null || (_lastGeoPosition?.Coordinate?.Latitude != newPoint.Latitude && _lastGeoPosition?.Coordinate?.Longitude != newPoint.Longitude))
+            {
+                if (myMap.Layers.Count >= 1)
+                {
+                    myMap.Layers.Clear();
+                }
+                
+
+                var MyLandmarks = new List<MapElement>();
+
+                var spaceNeedleIcon = new MapIcon
+                {
+                    Location = new Geopoint(newPoint),
+                    NormalizedAnchorPoint = new Point(0.5, 1.0),
+                    ZIndex = 0,
+                    Title = "Position"
+                };
+
+                MyLandmarks.Add(spaceNeedleIcon);
+
+                var LandmarksLayer = new MapElementsLayer
+                {
+                    ZIndex = 1,
+                    MapElements = MyLandmarks
+                };
+
+
+                
+                myMap.Layers.Add(LandmarksLayer);
+
+                if (center || _firstMeasure)
+                {
+                    myMap.Center = new Geopoint(newPoint);
+                    myMap.ZoomLevel = 20;
+                }
+            }
+            
+        }
+
+        private void BtnCenterToLocation_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            _firstMeasure = true;
+        }
+
+        private void BbtnCopyLocation_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
