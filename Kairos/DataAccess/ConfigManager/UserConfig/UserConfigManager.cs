a3f9f111205590877fff43ccc335506338747ffe
Robin <robin99re@gmail.com>
2019-09-13 15:13:01 +0200

    Implement the methods from the IUserConfigManager Inetrface

--- Kairos/DataAccess/ConfigManager/UserConfig/UserConfigManager.cs
+++ Kairos/DataAccess/ConfigManager/UserConfig/UserConfigManager.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Kairos.Business.Config;
+using Kairos.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,33 @@ using System.Threading.Tasks;
 
 namespace Kairos.DataAccess.ConfigManager.UserConfig
 {
-    public class UserConfigManager
+    public class UserConfigManager : IUserConfigManager
     {
+        public string PathToUserConfig { get; private set; }
+
+        public UserConfigManager()
+        {
+            PathToUserConfig = "";
+        }
+
+        public void DeleteUserConfig()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Data.UserConfig GetUserConfig()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadDefaultConfig()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveUserConfig()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
