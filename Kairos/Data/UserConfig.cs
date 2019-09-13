ebb6e778b43791c6b46f91546a55058339c9e27f
Robin <robin99re@gmail.com>
2019-09-13 11:40:30 +0200

    Possibility to create and manage the userconfig

---
+++ Kairos/Data/UserConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace Kairos.Data
+{
+    public class UserConfig
+    {
+        /// <summary>
+        /// The selected theme for the application
+        /// </summary>
+        public ElementTheme ElementTheme { get; set; }
+    }
+}
