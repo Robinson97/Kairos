a06ffe3e2d9e5b5c6617f2bf90320e463bfd56f2
Robin Eckhardt <Robin99re@gmail.com>
2019-09-29 15:12:49 +0200

    Backup Backgroundtask have been added

--- Kairos/UI/WorkOverview/WorkOverviewPage.xaml.cs
+++ Kairos/UI/WorkOverview/WorkOverviewPage.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.Media.Capture;
-using Windows.Storage;
-using Windows.UI.Xaml;
+using Windows.ApplicationModel.Background;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
@@ -25,13 +13,54 @@ namespace Kairos.UI.WorkOverview
     public sealed partial class WorkOverviewPage : Page
     {
         #region Propertys
+
         public WorkOverviewPageVM ViewModel { get; set; }
-        #endregion
+
+        #endregion Propertys
 
         public WorkOverviewPage()
         {
             this.InitializeComponent();
             this.DataContext = ViewModel = new WorkOverviewPageVM();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            foreach (var task in BackgroundTaskRegistration.AllTasks)
+            {
+                if (task.Value.Name == nameof(BackgroundTasks.Backup.BackupBackgroundTask))
+                {
+                    break;
+                }
+                else
+                {
+                    RegisterBackupBackgroundTask();
+                    break;
+                }
+            }
+
+            base.OnNavigatedTo(e);
+        }
+
+        private void RegisterBackupBackgroundTask()
+        {
+            var builder = new BackgroundTaskBuilder
+            {
+                Name = nameof(BackgroundTasks.Backup.BackupBackgroundTask),
+                TaskEntryPoint = "Kairos.BackgroundTasks.Backup.BackupBackgroundTask"
+            };
+
+            builder.SetTrigger(new SystemTrigger(SystemTriggerType.NetworkStateChange, false));
+            builder.AddCondition(new SystemCondition(SystemConditionType.InternetNotAvailable));
+            BackgroundTaskRegistration task = builder.Register();
+            task.Completed += Task_Completed;
+        }
+
+        private void Task_Completed(BackgroundTaskRegistration sender, BackgroundTaskCompletedEventArgs args)
+        {
+            Console.WriteLine("+++++++++++ Breaking News +++++++++++ ");
+            Console.WriteLine("+++++++++++ Kein Internet verfügbar +++++++++++ ");
+            Console.WriteLine("+++++++++++ Breaking News +++++++++++ ");
         }
     }
 }
