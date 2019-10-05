32bc292b8e2a9b37ce4a1323431415f155cdce34
Robin Eckhardt <Robin99re@gmail.com>
2019-10-05 17:14:41 +0200

    The AddJob PopUp have been moved into another location. A new Forms Window have been added

---
+++ Kairos/UI/NewJobDialog/NewJobDialogPageVM.cs
@@ -0,0 +1,53 @@
+﻿using GalaSoft.MvvmLight.Command;
+using Kairos.Core.Business.Base;
+using Kairos.Core.Data.Task;
+using Kairos.Core.UWP.Business.Navigation;
+using Kairos.Data.Task;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Kairos.UI.NewJobDialog
+{ 
+    public class NewJobDialogPageVM : ViewModelBase
+    {
+        private JobMediaTask _currentJob;
+
+        public JobMediaTask CurrentJob
+        {
+            get
+            {
+                return _currentJob;
+            }
+            set
+            {
+                _currentJob = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #region Commands
+        public ICommand CmdAddLocalFile { get; set; }
+
+        public ICommand CmdSaveTask { get; set; }
+
+        public ICommand CmdTakePictureFromWebcam { get; set; }
+        #endregion
+        
+
+        public NewJobDialogPageVM()
+        {
+            CurrentJob = new JobMediaTask();
+            CmdSaveTask = new RelayCommand<object>(SaveCurrentTask);
+            
+        }
+
+        private void SaveCurrentTask(object obj)
+        {
+            NavigationService.CurrentInstance.NavigateTo("WorkOverviewPage");
+        }
+    }
+}
